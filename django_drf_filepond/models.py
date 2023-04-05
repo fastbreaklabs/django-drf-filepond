e67404009dbd3e970a24612a79525ae4a2ef03e2
John Wu <origami7795@gmail.com>
2023-04-04 23:42:41 -0700

    Hack in s3 storage into django-drf-filepond

--- django_drf_filepond/models.py
+++ django_drf_filepond/models.py
@@ -16,22 +16,29 @@ import django_drf_filepond.drf_filepond_settings as local_settings
 from django.utils.functional import LazyObject
 from django_drf_filepond.storage_utils import _get_storage_backend
 
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class TemporaryS3Storage(S3Boto3Storage):
+    bucket_name = "glave-temp"
+
 
 LOG = logging.getLogger(__name__)
 
 BASE_DIR = local_settings.BASE_DIR
 try:
     from pathlib import Path
+
     if isinstance(BASE_DIR, Path):
         BASE_DIR = str(BASE_DIR)
 except ImportError:
     pass
 
 FILEPOND_UPLOAD_TMP = getattr(
-    local_settings, 'UPLOAD_TMP',
-    os.path.join(BASE_DIR, 'filepond_uploads'))
+    local_settings, "UPLOAD_TMP", os.path.join(BASE_DIR, "filepond_uploads")
+)
 
-FILEPOND_FILE_STORE_PATH = getattr(local_settings, 'FILE_STORE_PATH', None)
+FILEPOND_FILE_STORE_PATH = getattr(local_settings, "FILE_STORE_PATH", None)
 
 
 @deconstructible
@@ -48,13 +55,15 @@ class FilePondUploadSystemStorage(FileSystemStorage):
     """
 
     def __init__(self, **kwargs):
-        kwargs.update({
-            'location': FILEPOND_UPLOAD_TMP,
-        })
+        kwargs.update(
+            {
+                "location": FILEPOND_UPLOAD_TMP,
+            }
+        )
         super(FilePondUploadSystemStorage, self).__init__(**kwargs)
 
 
-storage = FilePondUploadSystemStorage()
+storage = TemporaryS3Storage()
 
 
 @deconstructible
@@ -71,20 +80,23 @@ class FilePondLocalStoredStorage(FileSystemStorage):
     """
 
     def __init__(self, **kwargs):
-        kwargs.update({
-            'location': FILEPOND_FILE_STORE_PATH,
-        })
+        kwargs.update(
+            {
+                "location": FILEPOND_FILE_STORE_PATH,
+            }
+        )
         super(FilePondLocalStoredStorage, self).__init__(**kwargs)
 
 
 class DrfFilePondStoredStorage(LazyObject):
-
     def _setup(self):
         # Work out which storage backend we need to use and then
         # instantiate it and assign it to self._wrapped
-        storage_module_name = getattr(local_settings, 'STORAGES_BACKEND', None)
-        LOG.debug('Initialising storage backend with storage module name [%s]'
-                  % storage_module_name)
+        storage_module_name = getattr(local_settings, "STORAGES_BACKEND", None)
+        LOG.debug(
+            "Initialising storage backend with storage module name [%s]"
+            % storage_module_name
+        )
         storage_backend = _get_storage_backend(storage_module_name)
         if not storage_backend:
             self._wrapped = FilePondLocalStoredStorage()
@@ -97,28 +109,27 @@ def get_upload_path(instance, filename):
 
 
 class TemporaryUpload(models.Model):
-
-    FILE_DATA = 'F'
-    URL = 'U'
+    FILE_DATA = "F"
+    URL = "U"
     UPLOAD_TYPE_CHOICES = (
-        (FILE_DATA, 'Uploaded file data'),
-        (URL, 'Remote file URL'),
+        (FILE_DATA, "Uploaded file data"),
+        (URL, "Remote file URL"),
     )
 
     # The unique ID returned to the client and the name of the temporary
     # directory created to hold file data
-    upload_id = models.CharField(primary_key=True, max_length=22,
-                                 validators=[MinLengthValidator(22)])
+    upload_id = models.CharField(
+        primary_key=True, max_length=22, validators=[MinLengthValidator(22)]
+    )
     # The unique ID used to store the file itself
-    file_id = models.CharField(max_length=22,
-                               validators=[MinLengthValidator(22)])
+    file_id = models.CharField(max_length=22, validators=[MinLengthValidator(22)])
     file = models.FileField(storage=storage, upload_to=get_upload_path)
     upload_name = models.CharField(max_length=512)
     uploaded = models.DateTimeField(auto_now_add=True)
-    upload_type = models.CharField(max_length=1,
-                                   choices=UPLOAD_TYPE_CHOICES)
-    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
-                                    blank=True, on_delete=models.CASCADE)
+    upload_type = models.CharField(max_length=1, choices=UPLOAD_TYPE_CHOICES)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
 
     def get_file_path(self):
         return self.file.path
@@ -129,42 +140,43 @@ class TemporaryUploadChunked(models.Model):
     # directory created to hold file data - this will be re-used in the
     # main TemporaryUpload record for this upload if/when all the chunks have
     # been successfully received and the upload is finalised.
-    upload_id = models.CharField(primary_key=True, max_length=22,
-                                 validators=[MinLengthValidator(22)])
+    upload_id = models.CharField(
+        primary_key=True, max_length=22, validators=[MinLengthValidator(22)]
+    )
     # The unique ID used to store the file chunks which are appended with
     # _<chunk_num>
-    file_id = models.CharField(max_length=22,
-                               validators=[MinLengthValidator(22)])
+    file_id = models.CharField(max_length=22, validators=[MinLengthValidator(22)])
     upload_dir = models.CharField(max_length=512, default=upload_id)
     last_chunk = models.IntegerField(default=0)
     offset = models.BigIntegerField(default=0)
     total_size = models.BigIntegerField(default=0)
-    upload_name = models.CharField(max_length=512, default='')
+    upload_name = models.CharField(max_length=512, default="")
     upload_complete = models.BooleanField(default=False)
     last_upload_time = models.DateTimeField(auto_now=True)
-    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
-                                    blank=True, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
 
 
 class StoredUpload(models.Model):
-
     # The unique upload ID assigned to this file when it was originally
     # uploaded (or retrieved from a remote URL)
-    upload_id = models.CharField(primary_key=True, max_length=22,
-                                 validators=[MinLengthValidator(22)])
+    upload_id = models.CharField(
+        primary_key=True, max_length=22, validators=[MinLengthValidator(22)]
+    )
     # The file name and path (relative to the base file store directory
     # as set by DJANGO_DRF_FILEPOND_FILE_STORE_PATH).
-    file = models.FileField(storage=DrfFilePondStoredStorage(),
-                            max_length=2048)
+    file = models.FileField(storage=DrfFilePondStoredStorage(), max_length=2048)
     uploaded = models.DateTimeField()
     stored = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
-                                    blank=True, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
 
     def get_absolute_file_path(self):
         fsp = local_settings.FILE_STORE_PATH
         if not fsp:
-            fsp = ''
+            fsp = ""
         return os.path.join(fsp, self.file.name)
 
 
@@ -174,17 +186,20 @@ class StoredUpload(models.Model):
 def delete_temp_upload_file(sender, instance, **kwargs):
     # Check that the file parameter for the instance is not None
     # and that the file exists and is not a directory! Then we can delete it
-    LOG.debug('*** post_delete signal handler called. Deleting file.')
+    LOG.debug("*** post_delete signal handler called. Deleting file.")
     if instance.file:
-        if (os.path.exists(instance.file.path) and
-                os.path.isfile(instance.file.path)):
+        if os.path.exists(instance.file.path) and os.path.isfile(instance.file.path):
             os.remove(instance.file.path)
 
-    LOG.debug('*** post_delete <%s> - Value of DELETE_UPLOAD_TMP_DIRS: %s'
-              % (instance.upload_id, str(local_settings.DELETE_UPLOAD_TMP_DIRS)))
+    LOG.debug(
+        "*** post_delete <%s> - Value of DELETE_UPLOAD_TMP_DIRS: %s"
+        % (instance.upload_id, str(local_settings.DELETE_UPLOAD_TMP_DIRS))
+    )
     if local_settings.DELETE_UPLOAD_TMP_DIRS:
         file_dir = os.path.join(storage.location, instance.upload_id)
-        if (os.path.exists(file_dir) and os.path.isdir(file_dir)):
+        if os.path.exists(file_dir) and os.path.isdir(file_dir):
             os.rmdir(file_dir)
-            LOG.debug('*** post_delete signal handler called. Deleting temp '
-                      'dir that contained file.')
+            LOG.debug(
+                "*** post_delete signal handler called. Deleting temp "
+                "dir that contained file."
+            )
